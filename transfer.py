5c02bbd9b66112436e08597c13123096a3df2c48
Ken Butler <Ken.Butler@gtri.gatech.edu>
2020-12-22 18:33:47 -0500

    Created search function for Spotify REST API queries

--- transfer.py
+++ transfer.py
@@ -1,27 +1,105 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
-import os
 import logging
+import os
+import sys
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+logging.basicConfig(
+    filename=os.path.join(os.getcwd(), "transfer.txt"),
+    filemode='w',
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 
 def get_credentials(directory: str = os.path.join(os.path.expanduser('~')), filename: str = '.spotify_credentials'):
     """
     Confirm Spotify credentials and set environment variables appropriately
-    Note that both SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET environment variables must be set
+    Both SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET environment variables must be set
     :param directory: Directory in which file exists
     :param filename: File that contains credentials
-    :return:
+    :return: Authorization Mananger
     """
-    logging.info("Acquiring credentials")
     fpath = os.path.join(directory, filename)
     if not os.path.exists(fpath):
         raise IOError("File path does not exist: {}".format(fpath))
     with open(fpath, 'r') as f:
         lines = f.readlines()
-        id = lines[0].replace('\n', '')
-        secret = lines[1].replace('\n', '')
-        redirect_uri = lines[2].replace('\n', '')
-        return id, secret, redirect_uri
+        username = lines[0].replace('\n', '')
+        id_num = lines[1].replace('\n', '')
+        secret = lines[2].replace('\n', '')
+        if len(lines) > 3:
+            redirect_uri = lines[3].replace('\n', '')
+        else:
+            redirect_uri = "http://localhost:8080"
+        logging.info("Acquired credentials")
+        return username, SpotifyOAuth(client_id=id_num, client_secret=secret, redirect_uri=redirect_uri)
+
+
+def create_playlist(auth_mgr: SpotifyOAuth, username: str, playlist_name: str):
+    """
+    Create Spotify playlist
+    :param auth_mgr: Authorization manager
+    :param username: Name of user
+    :param playlist_name: Name of playlist to create
+    :return: None
+    """
+    auth_mgr.scope = "playlist-modify-public"
+    sp = spotipy.Spotify(auth_manager=auth_mgr)
+    sp.user_playlist_create(username, playlist_name)
+    return
+
+
+def view_recently_played(auth_mgr: SpotifyOAuth):
+    auth_mgr.scope = "user-read-recently-played"
+    sp = spotipy.Spotify(auth_manager=auth_mgr)
+    print(sp.current_user_recently_played())
+    return
+
+
+def search(auth_mgr: SpotifyOAuth, track_name: str = None, artist: str = None, album: str = None):
+    """
+    Search for track ID
+    :param auth_mgr: Authorization manager
+    :param track_name: Name of track
+    :param artist: Name of artist
+    :param album: Name of album
+    :return: List of matching IDs
+    """
+    logging.info("Searching for '{}' by {}".format(track_name, artist))
+    sp = spotipy.Spotify(auth_manager=auth_mgr)
+
+    # TODO: Figure out if spotipy actually works with the the more advanced queries. For now just searching track name.
+    # query = ""
+    # for search_field in [track_name, artist, album]:
+    #     if search_field is not None:
+    #         query += search_field + " "
+    # query = quote(query.strip())  # Remove trailing white-spaces and codify for URL query
+
+    results = sp.search(track_name.strip())
+    id_matches = []
+    for n, listing in enumerate(results['tracks']['items']):
+        res_track = listing['name']
+        if track_name is not None and track_name != res_track:
+            continue
+        res_artist = listing['artists'][0]['name']
+        if artist is not None and artist != res_artist:
+            continue
+        res_album = listing['album']['name']
+        if album is not None and album != res_album:
+            continue
+        res_id = listing['id']
+        logging.info("\tFound '{}' by {} on album {} with ID = {}".format(res_track, res_artist, res_album, res_id))
+        id_matches.append(res_id)
+
+    if not id_matches:
+        raise RuntimeWarning("No matches found")
+    if len(id_matches) > 1:
+        raise RuntimeWarning("Ambiguous matches found")
+
+    return id_matches
 
 
 def main():
@@ -31,16 +109,16 @@ def main():
     """
 
     # Acquire credentials
-    client_id, client_secret, redirect_uri = get_credentials()
+    username, auth_mgr = get_credentials()
 
-    scope = "playlist-modify-public"
-    sp = spotipy.Spotify(
-        auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret,
-                                  redirect_uri="http://localhost:8080",
-                                  scope=scope))
+    # Search for "Feel Good Inc" by Gorillaz
+    id_matches = search(auth_mgr=auth_mgr, track_name="On Melancholy Hill", artist='Gorillaz')
 
-    sp.user_playlist_create('kenbutler4', 'testAPI')
-    # sp0 = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    # # Create playlist
+    # create_playlist(auth_mgr, username, 'testAPI')
+    #
+    # # View recently played
+    # view_recently_played(auth_mgr)
 
 
 if __name__ == '__main__':
